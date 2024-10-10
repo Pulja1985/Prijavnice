12b6c2dcbfbecf173aea2e4486ad74d055b81aeb
Hrvoje <hpuljic85@gmail.com>
2024-10-10 20:54:32 +0200

    Frontend u produkciju MVP

---
+++ Frontend/src/constants.js
@@ -0,0 +1,10 @@
+
+export const RouteNames = {
+    HOME: '/',
+
+    VOZAC_PREGLED: '/vozaci',
+    VOZAC_NOVI: '/vozaci/dodaj',
+    VOZAC_PROMJENA: '/vozaci/:sifra'
+}
+
+export const BACKEND_URL = 'https://krunokruno-001-site1.dtempurl.com/api/v1'
